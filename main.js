6541e0a6f8766d47bee37143134fca3a676a0033
acabidasales <acabidasales@gmail.com>
2023-11-08 02:56:39 +0100

    Feature: Añadido la funcion de modificar estilos CSS

--- main.js
+++ main.js
@@ -5,3 +5,7 @@ function change_contenidoHTML(){
 function change_atributoHTML(){
     document.getElementById("home_text").style.color = "red";
 }
+
+function change_css() {
+    document.getElementById("home_text").style.backgroundColor = "yellow";
+}
